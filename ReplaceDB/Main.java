6f8f9215b25c9c5fd42b31b0d4612253344bcf43
Andrew Pang <pang.134@osu.edu>
2022-03-27 18:49:28 -0400

    Add some error throwing

--- ReplaceDB/Main.java
+++ ReplaceDB/Main.java
@@ -1,11 +1,12 @@
 package ReplaceDB;
 
 import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Exception {
 
 		/**
 		*
@@ -29,8 +30,8 @@ public class Main {
 				.findFirst()
 					.ifPresent(ProcessHandle::destroy);
 			TimeUnit.SECONDS.sleep(3);
-			new File("\"" + "/Users/" + username() + "/.tkStrike/db/tkStrike30.mv.db" + "\"").delete();
-			executeCommand("open /Applications/tkStrikeGen2.app");
+			new File("//Users//" + username() + "//.tkStrike//db//tkStrike30.mv.db").delete();
+			executeCommand("open //Applications//tkStrikeGen2.app");
 			TimeUnit.SECONDS.sleep(25);
 			ProcessHandle
 					.allProcesses()
@@ -39,16 +40,22 @@ public class Main {
 					.ifPresent(ProcessHandle::destroy);
 			TimeUnit.SECONDS.sleep(3);
 			String h2 = "/Applications/tkStrikeGen2.app/Contents/Java/lib/h2-1.4.199.jar";
-			String h2Command = " org.h2.tools.RunScript ";
-			String url = "\"" + "jdbc:h2:/Users/" + username() + "/.tkStrike/db/tkStrike30" + "\"";
-			String subcategory = "\"" + directory() + "/Subcategory.sql" + "\"";
-			String thresholds = "\"" + directory() + "/Default_Category_Thresholds.sql" + "\"";
-			String gap = "\"" + directory() + "/Default_Gap.sql" + "\"";
-			System.out.println("java -cp " + h2 + h2Command + "-url " + url + " -user SA -script " + subcategory);
-			executeCommand("java -cp " + h2 + h2Command + "-url " + url + " -user SA -script " + subcategory);
-			executeCommand("java -cp " + h2 + h2Command + "-url " + url + " -user SA -script " + thresholds);
-			executeCommand("java -cp " + h2 + h2Command + "-url " + url + " -user SA -script " + gap);
-			executeCommand("open /Applications/tkStrikeGen2.app");
+			String h2Command = "org.h2.tools.RunScript";
+			Runtime runtime = Runtime.getRuntime();
+			String[] subcategory = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
+					+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script", directory() + "/subcategory.sql" };
+			String[] thresholds = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
+					+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script",
+					directory() + "/Default_Category_Thresholds.sql" };
+			String[] gap = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
+					+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script", directory() + "/Default_Gap.sql" };
+			runtime.exec(subcategory);
+			TimeUnit.SECONDS.sleep(1);
+			runtime.exec(thresholds);
+			TimeUnit.SECONDS.sleep(1);
+			runtime.exec(gap);
+			TimeUnit.SECONDS.sleep(1);
+			executeCommand("open //Applications//tkStrikeGen2.app");
 		} else if (getOperatingSystem().contains("Windows")) {
 			ProcessHandle
 					.allProcesses()
@@ -57,7 +64,7 @@ public class Main {
 					.ifPresent(ProcessHandle::destroy);
 			TimeUnit.SECONDS.sleep(3);
 			new File("C:\\Users" + username() + "\\AppData\\Local\\tkStrikeGen2\\app\\db\\tkStrike30.mv.db").delete();
-			executeCommand("C:\\Users" + username()
+			executeCommand("cmd.exe /c C:\\Users" + username()
 					+ "\\AppData\\Local\\tkStrikeGen2\\tkStrikeGen2.exe");
 			TimeUnit.SECONDS.sleep(25);
 			ProcessHandle
@@ -66,17 +73,45 @@ public class Main {
 					.findFirst()
 					.ifPresent(ProcessHandle::destroy);
 			TimeUnit.SECONDS.sleep(3);
-			String h2 = "\"" + "C:\\Users" + username() + "\\AppData\\Local\\tkStrikeGen2\\app\\lib\\h2-1.4.199.jar" + "\"";
-			String h2Command = " org.h2.tools.RunScript ";
-			String url = "\"" + "jdbc:h2:~\\AppData\\Local\\tkStrikeGen2\\app\\db\\tkStrike30" + "\"";
-			String subcategory = "\"" + directory() + "/Subcategory.sql" + "\"";
-			String thresholds = "\"" + directory() + "/Default_Category_Thresholds.sql" + "\"";
-			String gap = "\"" + directory() + "/Default_Gap.sql" + "\"";
-			System.out.println("java -cp " + h2 + h2Command + "-url " + url + " -user SA -script " + subcategory);
-			executeCommand("java -cp " + h2 + h2Command + "-url " + url + " -user SA -script " + subcategory);
-			executeCommand("java -cp " + h2 + h2Command + "-url " + url + " -user SA -script " + thresholds);
-			executeCommand("java -cp " + h2 + h2Command + "-url " + url + " -user SA -script " + gap);
-			executeCommand("C:\\Users" + username()
+			String h2 = "C:\\Users" + username() + "\\AppData\\Local\\tkStrikeGen2\\app\\lib\\h2-1.4.199.jar";
+			String h2Command = "org.h2.tools.RunScript";
+			String url = "jdbc:h2:~/AppData/Local/tkStrikeGen2/app/db/tkStrike30";
+			Runtime runtime = Runtime.getRuntime();
+			String[] subcategory = { "java", "-cp", h2, h2Command, "-url", url, "-user", "SA", "-script", directory() + "/subcategory.sql" };
+			String[] thresholds = { "java", "-cp", h2, h2Command, "-url", url, "-user", "SA", "-script",
+					directory() + "/Default_Category_Thresholds.sql" };
+			String[] gap = { "java", "-cp", h2, h2Command, "-url", url, "-user", "SA", "-script",
+					directory() + "/Default_Gap.sql" };
+			try {
+				Process proc = runtime.exec(subcategory);
+				TimeUnit.SECONDS.sleep(1);
+				runtime.exec(thresholds);
+				TimeUnit.SECONDS.sleep(1);
+				runtime.exec(gap);
+				TimeUnit.SECONDS.sleep(1);
+
+				BufferedReader stdInput = new BufferedReader(new
+					InputStreamReader(proc.getInputStream()));
+
+				BufferedReader stdError = new BufferedReader(new
+					InputStreamReader(proc.getErrorStream()));
+
+				// Read the output from the command
+				System.out.println("Here is the standard output of the command:\n");
+				String s = null;
+				while ((s = stdInput.readLine()) != null) {
+					System.out.println(s);
+				}
+
+				// Read any errors from the attempted command
+				System.out.println("Here is the standard error of the command (if any):\n");
+				while ((s = stdError.readLine()) != null) {
+					System.out.println(s);
+				}
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			executeCommand("cmd.exe /c C:\\Users" + username()
 					+ "\\AppData\\Local\\tkStrikeGen2\\tkStrikeGen2.exe");
 		} else {
 			System.out.println("YOU'VE DONE FUCKED UP, GO FUCK YOURSELF, I'M NOT MAKING THIS WORK FOR FUCKING UBUNTU/SOLARIS");
@@ -90,19 +125,29 @@ public class Main {
 
 	private static String username() {
 		String username = System.getProperty("user.name");
-		return username.replace(" ", "\\ ");
+		if (getOperatingSystem().contains("Mac")) {
+			username.replace(" ", "\\ ");
+		} else if (getOperatingSystem().contains("Windows")) {
+			username.replace(" ", "^ ");
+		}
+		return username;
 	}
 
 	private static String directory() {
 		String directory = System.getProperty("user.dir");
-		return directory.replace(" ", "\\ ");
+		if (getOperatingSystem().contains("Mac")) {
+			directory.replace(" ", "\\ ");
+		} else if (getOperatingSystem().contains("Windows")) {
+			directory.replace(" ", "^ ");
+		}
+		return directory;
 	}
 
 	private static void executeCommand(String command) {
 		try {
 			Process process = Runtime.getRuntime().exec(command);
 			process.waitFor();
-		} catch (IOException | InterruptedException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
