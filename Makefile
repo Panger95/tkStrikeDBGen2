44c1ffda17aa41bada6f427995c7f2a98fbc1635
Andrew Pang <pang.134@osu.edu>
2022-03-28 15:37:21 -0400

    Make this work only for windows

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all: build run
 
 build:
-	javac --release 9 ReplaceDB/Main.java
-	javac --release 9 ReplaceDB/Windows.java
+	javac --release 8 ReplaceDB/Main.java
+	javac --release 8 ReplaceDB/Windows.java
 	jar cfme Import\ Thresholds/ReplaceDB.jar Manifest.txt ReplaceDB.Main ReplaceDB/Main.class
 	jar cfme Import\ Thresholds/Windows.jar Manifest.txt ReplaceDB.Windows ReplaceDB/Windows.class
 
--- ReplaceDB/Main.java
+++ ReplaceDB/Main.java
@@ -21,48 +21,52 @@ public class Main {
 		*
 		**/
 
-		if (getOperatingSystem().contains("Mac")) {
-			ProcessHandle
-				.allProcesses()
-				.filter(p -> p.info().commandLine().map(c -> c.contains("tkStrikeGen2")).orElse(false))
-				.findFirst()
-					.ifPresent(ProcessHandle::destroy);
-			TimeUnit.SECONDS.sleep(3);
-			new File("//Users//" + username() + "//.tkStrike//db//tkStrike30.mv.db").delete();
-			executeCommand("open //Applications//tkStrikeGen2.app");
-			TimeUnit.SECONDS.sleep(25);
-			ProcessHandle
-				.allProcesses()
-				.filter(p -> p.info().commandLine().map(c -> c.contains("tkStrikeGen2")).orElse(false))
-				.findFirst()
-					.ifPresent(ProcessHandle::destroy);
-			TimeUnit.SECONDS.sleep(3);
-			String h2 = "/Applications/tkStrikeGen2.app/Contents/Java/lib/h2-1.4.199.jar";
-			String h2Command = "org.h2.tools.RunScript";
-			Runtime runtime = Runtime.getRuntime();
-			String[] subcategory = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
-					+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script", directory() + "/subcategory.sql" };
-			String[] thresholds = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
-					+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script",
-					directory() + "/Default_Category_Thresholds.sql" };
-			String[] gap = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
-					+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script", directory() + "/Default_Gap.sql" };
-			TimeUnit.SECONDS.sleep(3);
-			runtime.exec(subcategory);
-			TimeUnit.SECONDS.sleep(1);
-			runtime.exec(thresholds);
-			TimeUnit.SECONDS.sleep(1);
-			runtime.exec(gap);
-			TimeUnit.SECONDS.sleep(1);
-			executeCommand("open //Applications//tkStrikeGen2.app");
-		} else if (getOperatingSystem().contains("Windows")) {
-			Runtime.getRuntime().exec("taskkill /F /IM tkStrikeGen2.exe");
-			TimeUnit.SECONDS.sleep(3);
-			new File("C:\\Users\\" + username() + "\\AppData\\Local\\tkStrikeGen2\\app\\db\\tkStrike30.mv.db").delete();
-			executeCommand("C:\\Users\\" + username() + "\\AppData\\Local\\tkStrikeGen2\\tkStrikeGen2.exe");
-		} else {
-			System.out.println("YOU'VE DONE FUCKED UP, GO FUCK YOURSELF, I'M NOT MAKING THIS WORK FOR FUCKING UBUNTU/SOLARIS");
-		}
+		// if (getOperatingSystem().contains("Mac")) {
+		// 	ProcessHandle
+		// 		.allProcesses()
+		// 		.filter(p -> p.info().commandLine().map(c -> c.contains("tkStrikeGen2")).orElse(false))
+		// 		.findFirst()
+		// 			.ifPresent(ProcessHandle::destroy);
+		// 	TimeUnit.SECONDS.sleep(3);
+		// 	new File("//Users//" + username() + "//.tkStrike//db//tkStrike30.mv.db").delete();
+		// 	executeCommand("open //Applications//tkStrikeGen2.app");
+		// 	TimeUnit.SECONDS.sleep(25);
+		// 	ProcessHandle
+		// 		.allProcesses()
+		// 		.filter(p -> p.info().commandLine().map(c -> c.contains("tkStrikeGen2")).orElse(false))
+		// 		.findFirst()
+		// 			.ifPresent(ProcessHandle::destroy);
+		// 	TimeUnit.SECONDS.sleep(3);
+		// 	String h2 = "/Applications/tkStrikeGen2.app/Contents/Java/lib/h2-1.4.199.jar";
+		// 	String h2Command = "org.h2.tools.RunScript";
+		// 	Runtime runtime = Runtime.getRuntime();
+		// 	String[] subcategory = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
+		// 			+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script", directory() + "/subcategory.sql" };
+		// 	String[] thresholds = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
+		// 			+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script",
+		// 			directory() + "/Default_Category_Thresholds.sql" };
+		// 	String[] gap = { "java", "-cp", h2, h2Command, "-url", "jdbc:h2:/Users/" + username()
+		// 			+ "/.tkStrike/db/tkStrike30", "-user", "SA", "-script", directory() + "/Default_Gap.sql" };
+		// 	TimeUnit.SECONDS.sleep(3);
+		// 	runtime.exec(subcategory);
+		// 	TimeUnit.SECONDS.sleep(1);
+		// 	runtime.exec(thresholds);
+		// 	TimeUnit.SECONDS.sleep(1);
+		// 	runtime.exec(gap);
+		// 	TimeUnit.SECONDS.sleep(1);
+		// 	executeCommand("open //Applications//tkStrikeGen2.app");
+		// } else if (getOperatingSystem().contains("Windows")) {
+		// 	Runtime.getRuntime().exec("taskkill /F /IM tkStrikeGen2.exe");
+		// 	TimeUnit.SECONDS.sleep(3);
+		// 	new File("C:\\Users\\" + username() + "\\AppData\\Local\\tkStrikeGen2\\app\\db\\tkStrike30.mv.db").delete();
+		// 	executeCommand("C:\\Users\\" + username() + "\\AppData\\Local\\tkStrikeGen2\\tkStrikeGen2.exe");
+		// } else {
+		// 	System.out.println("YOU'VE DONE FUCKED UP, GO FUCK YOURSELF, I'M NOT MAKING THIS WORK FOR FUCKING UBUNTU/SOLARIS");
+		// }
+		Runtime.getRuntime().exec("taskkill /F /IM tkStrikeGen2.exe");
+		TimeUnit.SECONDS.sleep(3);
+		new File("C:\\Users\\" + username() + "\\AppData\\Local\\tkStrikeGen2\\app\\db\\tkStrike30.mv.db").delete();
+		executeCommand("C:\\Users\\" + username() + "\\AppData\\Local\\tkStrikeGen2\\tkStrikeGen2.exe");
 	}
 
 	private static String getOperatingSystem() {
@@ -80,20 +84,19 @@ public class Main {
 		return username;
 	}
 
-	private static String directory() {
-		String directory = System.getProperty("user.dir");
-		if (getOperatingSystem().contains("Mac")) {
-			directory.replace(" ", "\\ ");
-		} else if (getOperatingSystem().contains("Windows")) {
-			directory.replace(" ", "^ ");
-		}
-		return directory;
-	}
+	// private static String directory() {
+	// 	String directory = System.getProperty("user.dir");
+	// 	if (getOperatingSystem().contains("Mac")) {
+	// 		directory.replace(" ", "\\ ");
+	// 	} else if (getOperatingSystem().contains("Windows")) {
+	// 		directory.replace(" ", "^ ");
+	// 	}
+	// 	return directory;
+	// }
 
 	private static void executeCommand(String command) {
 		try {
-			Process process = Runtime.getRuntime().exec(command);
-			process.waitFor();
+			Runtime.getRuntime().exec(command);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
